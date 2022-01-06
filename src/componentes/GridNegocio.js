6bf213845ba91ac5347b409b459323f6f4e31649
josenathaniel <josenathanielc@gmail.com>
2022-01-06 14:58:25 -0600

    dfsd

--- src/componentes/GridNegocio.js
+++ src/componentes/GridNegocio.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { NegocioItem } from './NegocioItem'
 import './styles.css';
-import { boton } from './boton';
+
 
 
  const GridNegocio =  () => {
